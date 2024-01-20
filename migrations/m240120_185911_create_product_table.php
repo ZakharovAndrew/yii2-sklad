2d18e870295841325bf7f772803ea2ad476b3aa3
Zakharov Andrey <37038505+ZakharovAndrew@users.noreply.github.com>
2024-01-20 22:56:34 +0300

    fix

--- migrations/m240120_185911_create_product_table.php
+++ migrations/m240120_185911_create_product_table.php
@@ -15,9 +15,9 @@ class m240120_185911_create_product_table extends Migration
                 'id' => $this->primaryKey(),
                 'name' => $this->string(500)->notNull(),
                 'images' => $this->text(),
-                'link' => $this->string(500)->notNull(),
+                'link' => $this->string(500),
                 'product_category_id' => $this->integer(),
-                'comments' => $this->string(500)->notNull(),
+                'comments' => $this->string(500),
                 'good' => $this->integer(),
                 'bad' => $this->integer(),
             ]
