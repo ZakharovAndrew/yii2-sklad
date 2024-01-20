083bdaeecab56981b6ee289e9a4711f3e7b7708c
Zakharov Andrey <37038505+ZakharovAndrew@users.noreply.github.com>
2024-01-20 21:12:49 +0300

    init

---
+++ migrations/m240120_185911_create_product_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Handles the creation of table `product`.
+ */
+class m240120_185911_create_product_table extends Migration
+{
+    public function up()
+    {
+        $this->createTable(
+            'product',
+            [
+                'id' => $this->primaryKey(),
+                'name' => $this->string(500)->notNull(),
+                'images' => $this->text(),
+                'link' => $this->string(500)->notNull(),
+                'product_category_id' => $this->integer(),
+                'comments' => $this->string(500)->notNull(),
+                'good' => $this->integer(),
+                'bad' => $this->integer(),
+            ]
+        );
+    }
+
+    public function down()
+    {
+        $this->dropTable('product');
+    }
+}
